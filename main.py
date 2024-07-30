93670753627fad45f69c9184d6a353338249bedf
Prithvi Khawas <35435596-prithvikhawas74@users.noreply.replit.com>
2024-07-30 16:37:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#simple calculator using simple method
+
+
+
+p = 165
+q = 5
+
+
+add = p + q
+subtract = p - q
+mul = p * q
+div = p // q
+
+
+print("The addition of two numbers:",add);
+print("The subtraction of two numbers:",subtract);
+print("The multiplication of two numbers:",mul);
+print("The division of two numbers:",div);
+
