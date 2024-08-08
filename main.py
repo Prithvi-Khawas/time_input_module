78df157c6c6f33a221ccbc859b74cd84421ad183
Prithvi-Khawas <94523792+Prithvi-Khawas@users.noreply.github.com>
2024-08-08 15:54:15 +0000

    user_input module
    user input

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
-#simple calculator using simple method
+import time
+
+t = int(input("Enter the time : "))
+
+        
+current_time = time.strftime('%H:%M:%S')
+print(current_time)
+current_hour = int(time.strftime( '%H'))
+print(current_hour)
+current_minute = int(time.strftime( '%M'))
+print(current_minute)
+current_seconds = int(time.strftime( '%S'))
+print(current_seconds)
+
+
+if current_hour < 12:
+    print("Good Morning")
+elif current_hour <= 12 and current_hour < 16:
+    print("Good Afternoon")  
+else:
+    print("Good Evening")
+  
 
 
 
-p = 165
-q = 5
 
 
-add = p + q
-subtract = p - q
-mul = p * q
-div = p // q
 
 
-print("The addition of two numbers:",add);
-print("The subtraction of two numbers:",subtract);
-print("The multiplication of two numbers:",mul);
-print("The division of two numbers:",div);
+
+
+
 
